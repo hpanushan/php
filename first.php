00eee0ccde20e656148f40e858cb0c53cd43d5ad
anushanram <hpanushan@gmail.com>
2019-05-24 21:48:19 +0530

    Created basic php files

---
+++ first.php
@@ -0,0 +1,15 @@
+<?php 
+
+// This is the start
+echo "Hello world" ;
+echo "<br>";
+echo "Hello world" ;
+
+/* Multiline comment
+*/
+echo "<br>";
+echo "First name" . " Second name";
+echo "<br>";
+echo (7*3);
+
+?>
---
+++ variables.php
@@ -0,0 +1,20 @@
+
+<?php 
+
+// Variables
+
+$name1 = "Anushan";
+$name2 = "Nimesh";
+$name3 = 'david';
+
+$number = 12;
+
+$number = 23;
+
+
+
+
+echo $number**2;
+
+
+ ?>
