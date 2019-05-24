c6d8b5a0598fef05876ec4e333857fccb8a17194
anushanram <hpanushan@gmail.com>
2019-05-24 22:02:07 +0530

    Used variables inside the double coutes

---
+++ strings.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Strings
+
+$fName = "Anushan";
+$lName = " Ramesh";
+$age = 26;
+
+echo $fName.$lName;
+
+echo "</br>";
+
+echo "Hello {$fName} {$lName}, you are {$age}s years old!"; 
+
+ ?>
