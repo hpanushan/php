b651f6b3c5af968a3f09b78812ff48881dc345fb
anushanram <hpanushan@gmail.com>
2019-05-24 23:34:26 +0530

    Created integers and its functions

---
+++ integers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Integers</title>
+</head>
+<body>
+
+	<?php 
+
+		$num1 = 12;
+		$num2 = 7;
+
+	 ?>
+
+	 <h2>Some useful integer functions</h2>
+	 Square-Root : <?php echo sqrt(23); ?>	
+	 <br>
+	 Power : <?php echo pow(3, 3); ?>
+	 <br>
+	 Absolute value : <?php echo abs(-4.3); ?>
+	 <br>
+	 Random number : <?php echo rand(-100,100); ?>
+
+
+</body>
+</html>
