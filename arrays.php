68eb53ed7d897eba84fbe829745df6861da703cc
anushanram <hpanushan@gmail.com>
2019-05-25 00:10:27 +0530

    Creating arrays and associative arrays

---
+++ arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arrays</title>
+</head>
+<body>
+
+<?php 
+
+	$var = array('Apple','Orange','Mango','Banana');
+	$var[0] = 12;
+
+ ?>
+
+<pre>
+	<?php echo print_r($var); ?>
+</pre>
+
+</body>
+</html>
---
+++ associativeArray.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Associative Array</title>
+</head>
+<body>
+
+<?php 
+	
+	$var = array(1 => "Anushan", "lName" => "Ramesh");
+
+ ?>
+
+<pre>
+	
+	<?php print_r($var); ?> <br>
+	<?php echo $var[1]; ?>
+
+</pre>
+
+</body>
+</html>
