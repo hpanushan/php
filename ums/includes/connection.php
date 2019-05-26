7959f65e1675e637933c805b983c295abf13ea39
anushanram <hpanushan@gmail.com>
2019-05-26 19:12:12 +0530

    connection to the mysql database

---
+++ ums/includes/connection.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$connection = mysqli_connect('localhost', 'root', '', 'userdb');
+
+	if (mysqli_connect_errno()){
+		die('Database connection failed ' . mysqli_connect_error);
+	}
+	else{
+		echo "Connection is successful";
+	}
+
+ ?>
---
+++ ums/index.php
@@ -0,0 +1,11 @@
+<?php include('includes/connection.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<?php mysqli_close($connection); ?>
