3bbbe609a23225f9c59131c0f9ea1d23d21a9437
anushanram <hpanushan@gmail.com>
2019-05-26 12:17:32 +0530

    Used foreach loop

---
+++ forEach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>For Each</title>
+</head>
+<body>
+	<?php 
+		$array = array('nimal','sunil','gayani','geeshani','kalani','mahesh');
+
+		foreach ($array as $value) {
+			echo $value."<br>";
+			
+		}
+	 ?>
+
+</body>
+</html>
