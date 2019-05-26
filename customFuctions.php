d1043c9395ebbb883e767b22d413bc577ca2c445
anushanram <hpanushan@gmail.com>
2019-05-26 12:42:50 +0530

    Created functions

---
+++ customFuctions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Custom Functions</title>
+</head>
+<body>
+
+<?php 
+
+echo average(12,13.123123);
+
+function average($num1,$num2){
+	return ($num1+$num2)/2;
+}
+
+
+
+
+ ?>
+
+</body>
+</html>
