fd0a47703ebf3d7c4b9b7c3af13e30cb0accac9b
anushanram <hpanushan@gmail.com>
2019-05-26 11:39:24 +0530

    Created while loop

---
+++ whileLoop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>While Loop</title>
+</head>
+<body>
+
+	<?php 
+		$num = 1;
+		while ($num<=10){
+			echo $num."<br>";
+			$num++;
+		}
+	 ?>
+
+</body>
+</html>
