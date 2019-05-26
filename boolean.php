0c39958a9a5299472ba20504df70dd92e28d6431
anushanram <hpanushan@gmail.com>
2019-05-26 11:19:48 +0530

    Created boolean variables and used them

---
+++ boolean.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Boolean</title>
+</head>
+<body>
+
+<?php 
+
+	$var1 = true;
+	$var2 = false;
+
+ ?>
+
+ <?php echo "Variable 1 : {$var1}"; ?> <br>
+ <?php echo "Variable 2 : {$var2}"; ?> <br>
+
+</body>
+</html>
